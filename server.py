30cd333ef7d306a97b1c0269b6cd75814016c1c1
mcproject24 <114114623+mcproject24@users.noreply.github.com>
2022-09-21 16:25:15 -0700

    Initial Commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, Response
+import sys
+import json
+import os
+app = Flask(__name__)
+
+@app.route("/save_image", methods = ['POST'])
+def save_image():
+    if request.method == 'POST':
+        photo = request.files["photo"]
+        folder = request.form["description"]
+
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            photo.save("./"+folder+"/1.jpg")
+        else:
+            count = len(os.listdir(folder)) + 1
+            photo.save("./"+folder+"/"+str(count)+".jpg")
+            
+        
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
